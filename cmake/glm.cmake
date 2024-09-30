1109e933649f203d3c966ef58b54da9a9058f86b
Axel Paris <aparis@adobe.com>
2024-09-30 16:28:37 +0200

    cmake version

--- cmake/glm.cmake
+++ cmake/glm.cmake
@@ -5,7 +5,7 @@ include(FetchContent)
 FetchContent_Declare(
     glm
     GIT_REPOSITORY https://github.com/g-truc/glm.git
-    GIT_TAG 0.9.9.8
+    GIT_TAG 1.0.1
 )
 
 FetchContent_MakeAvailable(glm)
